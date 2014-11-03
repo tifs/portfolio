e6e81bcc0c79be88f77b96ab62ea3cece02b1545
Tif Slama <tif.slama@gmail.com>
2014-11-02 22:12:42 -0800

    videos are responvise and always 16:9 aspect ratio

---
+++ public/javascripts/portfolio.js
@@ -0,0 +1,23 @@
+// $(document).ready(function() {  
+// 	var stickyNavTop = $('.main-nav').offset().top;  
+  
+// 	var stickyNav = function(){  
+// 	var scrollTop = $(window).scrollTop();  
+       
+// if (scrollTop > stickyNavTop) {   
+//   $('.main-nav').addClass('sticky');  
+// 	} else {  
+//   $('.main-nav').removeClass('sticky');   
+// 	}  
+// };  
+  
+// stickyNav();  
+  
+// $(window).scroll(function() {  
+//   stickyNav();  
+// 	});  
+// });  
+
+$(document).ready(function() {
+  $('.header').scrollToFixed();
+  });
