3b82cc933634d8e5e5e0ec172ccf433c6747591d
Tif Slama <tif.slama@gmail.com>
2014-11-16 14:30:23 -0800

    updated with an svg logo

--- public/javascripts/portfolio.js
+++ public/javascripts/portfolio.js
@@ -31,3 +31,30 @@ function kimonoCallback(data) {
   //     $(this).removeClass('hover');
   //   }
   // );
+
+
+// CODE FOR COOL HOVER - from @er_d0s
+// var holder;
+// $(document).ready(function(){
+//   $(".weirdHover").mousedown(function(e){
+//     $(this).stopTime();
+//     e.preventDefault();
+//     window.location=$(this).attr("href");
+//   });
+//   $(".weirdHover").hover(function(){
+//     holder = $(this).html();
+//     var t = holder;
+//     var len = t.length;
+//     $(this).everyTime(20,function(i){
+//       var newstring = "";
+//       for (var i=0;i<len;i++)
+//       {
+//         newstring = newstring + String.fromCharCode(97 + Math.round(Math.random() * 25));
+//       }
+//       $(this).html(newstring);
+//     });
+//   },function(){
+//     $(this).stopTime();
+//     $(this).html(holder);
+//   });
+// });
